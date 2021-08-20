8c1fd4f2f70db4d5366be02014250068980f0693
Agrigorjevs-FG <andrejs.grigorjevs@futuregames.nu>
2021-08-20 14:17:44 +0200

    Door prefab added

---
+++ Point-and-Click-Mystery/Assets/Scripts/DoubleDoorAnimator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleDoorAnimator : MonoBehaviour
+{
+    [SerializeField] private Animator doubleDoorAnimatorControllerRight;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            doubleDoorAnimatorControllerRight.SetBool("Player on Trigger", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            doubleDoorAnimatorControllerRight.SetBool("Player on Trigger", false);
+        }
+    }
+}
